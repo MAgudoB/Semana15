68dd3019bfa6f03a6249b035ee19d9f2fed4a06d
osanfer <oscarsanchezfer@gmail.com>
2019-03-12 20:35:17 +0100

    started triggers

--- js/Class/Triggers/Switch.js
+++ js/Class/Triggers/Switch.js
@@ -1,1 +1,10 @@
-MainClass.prototype = Object.create(MainClass.prototype);
+Trigger.prototype = Object.create(Trigger.prototype);
+function Switch(target) {
+    Trigger.call(this, x, y, imgOff, imgOn);
+    this.target = target;
+
+    this.trigger = function () {
+        Trigger.call()
+        this.targe.trigger(this.triggered);
+    }
+}
