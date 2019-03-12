3967df702ac0efe2c928303259b7340e65f66b77
fullbatterygames <fullbatterygames@gmail.com>
2019-03-12 16:08:37 +0100

    personaje

--- js/Class/Player2.js
+++ js/Class/Player2.js
@@ -1,8 +1,24 @@
 function Player2(){
     this.id;
-    this.posX;
-    this.posY;
+    this.x;
+    this.y;
+
+    this.img=new Image();
+    this.img.src="img/players/player1.png";
 
     
+
+
+
+
+    this.move=function(){
+
+
+    }
+
+
+    this.paint=function(){
+        
+    }
 
 }
