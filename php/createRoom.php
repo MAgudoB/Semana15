ec3513cd1f7413008eab9a8645b5cf6b5768fe31
Miguel <magudob@gmail.com>
2019-03-13 21:52:12 +0100

    a la tercera...

--- php/createRoom.php
+++ php/createRoom.php
@@ -5,16 +5,16 @@
     $result = $mysqli->query($query);
     $row = $result->fetch_assoc();
     //Si ya hay Salas
-    if ($row['roomId'] != "") {
+    if ($row['RoomId'] != "") {
       //Si no es la primera sala, vamos añadiendo numeros
       $nextRoom = $row['roomId']+1;
-      $query = "INSERT INTO `Rooms` (RoomId, UserId,Active) VALUES ('".$nextRoom."','".$_GET['UserId']."',1)";
+      $query = "INSERT INTO `Rooms` (RoomId,UserId,Active) VALUES ('".$nextRoom."','".$_POST['UserId']."',1)";
       $result = $mysqli->query($query);
       echo $nextRoom;
     }
     //Si es la primera sala, la creamos
     else{
-        $query = "INSERT INTO `Rooms` (RoomId, UserId) VALUES (1,'".$_GET['UserId']."',1)";
+        $query = "INSERT INTO `Rooms` (RoomId,UserId,Active) VALUES (1,'".$_POST['UserId']."',1)";
         $result = $mysqli->query($query);
         echo "1";
     }
--- php/joinRoom.php
+++ php/joinRoom.php
@@ -1,24 +1,24 @@
 <?php
     $mysqli = new mysqli("localhost", "semana14", "semana14", "game");
 	//Comprobar si existe la sala
-    $query = "SELECT RoomId FROM `Rooms` WHERE RoomId = '".$_GET['RoomId']."'";
+    $query = "SELECT RoomId FROM `Rooms` WHERE RoomId = '".$_POST['RoomId']."'";
     //Room tiene Id, RoomId, UserId	
     $result = $mysqli->query($query);
     $row = $result->fetch_assoc();
     //Si la sala existe
 	if ($result->num_rows > 0) {
 		//Ahora hay que comprobar si ya tiene los dos usuarios
-		$query2 = "SELECT COUNT(*) as nPlayers FROM `Rooms` WHERE roomId = '".$_GET['RoomId']."'";
+		$query2 = "SELECT COUNT(*) as nPlayers FROM `Rooms` WHERE roomId = '".$_POST['RoomId']."'";
 		$result2 = $mysqli->query($query2);
 		$row = $result->fetch_assoc();
 		$playersInRoom = $row['nPlayers'];
 		//Si estan los dos, comprobar si eres uno de los dos
 		if ($playersInRoom == 2) {
-			$queryFullRoom = "SELECT RoomId FROM `Rooms` WHERE RoomId = '".$_GET['RoomId']."' AND UserId = '".$_GET['UserId']."'";
+			$queryFullRoom = "SELECT RoomId FROM `Rooms` WHERE RoomId = '".$_POST['RoomId']."' AND UserId = '".$_POST['UserId']."'";
 			$resultFullRoom = $mysqli->query($queryFullRoom);
 			//Si eres puedes jugar
 			if ($result->num_rows > 0) {
-				$queryUpdateActive = "UPDATE `Rooms` SET Active=1 WHERE RoomId = '".$_GET['RoomId']."' AND UserId = '".$_GET['UserId']."'";
+				$queryUpdateActive = "UPDATE `Rooms` SET Active=1 WHERE RoomId = '".$_POST['RoomId']."' AND UserId = '".$_POST['UserId']."'";
 				$resultUpdateActive = $mysqli->query($queryUpdateActive);
 				echo $_GET['RoomId'];
 			}
@@ -29,7 +29,7 @@
 		}
 		//Si no está llena se te añade.
 		else {
-			$queryInsert = "INSERT INTO `Rooms` (RoomId, UserId,Active) VALUES ('".$_GET['RoomId']."','".$_GET['UserID']."',1)";
+			$queryInsert = "INSERT INTO `Rooms` (RoomId, UserId,Active) VALUES ('".$_POST['RoomId']."','".$_POST['UserID']."',1)";
 			$result = $mysqli->query($queryInsert);
 			echo $_GET['RoomId'];
 		}
